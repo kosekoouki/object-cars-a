5a343bdda47f9cbbc8cf49700ec31b1b34e7e5e3
utenma <tenma.ueno@tokyo.supersoftware.co.jp>
2025-04-22 17:37:41 +0900

    sessionの追加

--- Q5/session_init.php
+++ Q5/session_init.php
@@ -1,7 +1,8 @@
 <?php
 $_SESSION['game']=['car'=>"",'time'=>0,'round'=>1,'win_count'=>0];
-$_SESSION['honda']=['name'=>'honda','price'=>0,'distance'=>0,'spead'=>0,'time'=>0,'round'=>1,'win_count'=>0];
-$_SESSION['nissan']=['name'=>'nissan','price'=>0,'distance'=>0,'spead'=>0,'time'=>0,'round'=>1,'win_count'=>0];
-$_SESSION['ferrari']=['name'=>'ferrari','price'=>0,'distance'=>0,'spead'=>0,'time'=>0,'round'=>1,'win_count'=>0];
-$_SESSION['toyota']=['name'=>'toyota','price'=>0,'distance'=>0,'spead'=>0,'time'=>0,'round'=>1,'win_count'=>0];
+$_SESSION['honda']=['name'=>'honda','price'=>0,'acceleration'=>0,'decceleration'=>0,'distance'=>0,'spead'=>0];
+$_SESSION['nissan']=['name'=>'nissan','price'=>0,'acceleration'=>0,'decceleration'=>0,'distance'=>0,'spead'=>0];
+$_SESSION['ferrari']=['name'=>'ferrari','price'=>0,'acceleration'=>0,'decceleration'=>0,'distance'=>0,'spead'=>0];
+$_SESSION['toyota']=['name'=>'toyota','price'=>0,'acceleration'=>0,'decceleration'=>0,'distance'=>0,'spead'=>0];
+$_SESSION['ranking']=array();
 ?>
