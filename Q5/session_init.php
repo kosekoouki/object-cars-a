4b134618656249d9e2f1f8b31ad9fd455b75d8f0
utenma <tenma.ueno@tokyo.supersoftware.co.jp>
2025-04-22 15:13:37 +0900

    sessionファイルを作成しました。

---
+++ Q5/session_init.php
@@ -0,0 +1,7 @@
+<?php
+$_SESSION['game']=['car'=>"",'time'=>0,'round'=>1,'win_count'=>0];
+$_SESSION['honda']=['name'=>'honda','price'=>0,'distance'=>0,'spead'=>0,'time'=>0,'round'=>1,'win_count'=>0];
+$_SESSION['nissan']=['name'=>'nissan','price'=>0,'distance'=>0,'spead'=>0,'time'=>0,'round'=>1,'win_count'=>0];
+$_SESSION['ferrari']=['name'=>'ferrari','price'=>0,'distance'=>0,'spead'=>0,'time'=>0,'round'=>1,'win_count'=>0];
+$_SESSION['toyota']=['name'=>'toyota','price'=>0,'distance'=>0,'spead'=>0,'time'=>0,'round'=>1,'win_count'=>0];
+?>
