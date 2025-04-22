53c5d5db82e93723f1284ed0ded6a0bebc3b3368
Ouki Koseko <ouki.koseko@tokyo.supersoftware.co.jp>
2025-04-22 14:47:41 +0900

    hondaクラス作成済み

---
+++ Q1～Q4/classes/honda.php
@@ -0,0 +1,14 @@
+<?php
+require_once("car.php");
+class Honda extends Car{
+    public $name = "Honda";
+    public $priceRange = ['min'=>500,'max'=>600];
+    public $acceleration = 4.0;
+    public $deceleration = 8.0;
+    
+    public function __construct($capacity = 7){
+        parent::__construct($capacity);
+    }
+    
+}
+?>
