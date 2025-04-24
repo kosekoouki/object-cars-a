5c177a5797314b218c039c7b01748d21381b14bc
sayaka-shiba <sayakalovenatsu@icluod.com>
2025-04-24 15:48:16 +0900

    結果画面修正

--- Q5/view/result_view.php
+++ Q5/view/result_view.php
@@ -12,9 +12,9 @@
             text-align: center;
             padding-top: 50px;
             color: #333;
-            background-color:rgb(244, 192, 50); 
-            margin: 0;              
-            padding: 0;              
+            background-color:rgb(148, 203, 245); 
+            margin: 10;              
+            padding: 50;              
             width: 100vw;            
             height: 100vh;           
             overflow: hidden;        /* コンテンツがはみ出さないようにする（必要に応じて） */
@@ -23,9 +23,8 @@
       
         h1 {
             font-size: 70px;
-            margin-bottom: 30px;
-            margin-top: 10px;
-            
+            margin-bottom: 50px;
+            margin-top: 60px;    
         }
 
         .result-info {
@@ -37,55 +36,33 @@
             font-size: 90px;
             font-weight: bold;
             color: <?= $_SESSION['game']['win_count'] == 2 ? '#28a745' : 'red' ?>;
-            margin-bottom: 30px; 
-            
+            margin-bottom: 0px;  
         }
 
         .finish-button {
-            padding: 12px 30px;                
+            padding: 15px 30px;     
+            margin-top: 60px;           
             font-size: 16px;                
             background-color:rgb(10, 193, 65);
             color: white; 
-            border: none;                    
+            border: none;
             color: white;                   
             font-weight: bold;                 
             cursor: pointer;
             transition: all 0.3s ease;   
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-
-        }
-        
-          /* コンテナにflexboxを設定して右寄せ */
-          .container {
-            display: flex;
-            justify-content: flex-end;  /* 画像を右側に寄せる */
-            align-items: center;        /* 垂直方向で中央寄せ */
-            height: 10vh;              /* コンテナの高さを画面全体に */
-        }
-
-            img {
-            width: 30%;  
-            height: auto;  
-
         }
 </style>
-
 
 <body>
     <div class="result-box">
         <h1>結果</h1>
         <div class="result-info">ラウンド数：<?= $_SESSION['game']['round'] ?></div>
         <div class="result-info">勝利数：<?= $_SESSION['game']['win_count'] ?></div>
-        <div class="result-outcome"><?= $_SESSION['game']['win_count'] == 2 ? "勝ち" : "負け" ?></div>
+        <div class="result-outcome"><?= $_SESSION['game']['win_count'] == 2 ? "🎉勝ち🎉" : "😢負け😢" ?></div>
         <form action="../controller.php" method="post">
-        <input class="finish-button" type="submit" name="finish" value="終了">
+        <input class="finish-button" type="submit" name="finish" value="🚗終了🚗">
         </form>
     </div>
-
-    
-    <div class="container">
-        <img src="https://cdn.pixabay.com/photo/2013/07/13/10/30/man-157378_1280.png">
-    </div>
-
 </body>
 </html>
