6f1c8c0ab456db506408402ae133a56878e6a8d6
Ouki Koseko <ouki.koseko@tokyo.supersoftware.co.jp>
2025-04-24 13:56:02 +0900

    スタート画面画像追加

--- Q5/view/start_view.php
+++ Q5/view/start_view.php
@@ -14,13 +14,13 @@
             height: 100vh;           
             margin: 0;               
             font-family: Arial, sans-serif;
-            background: #ff1414;
+            background:rgb(225, 89, 51);
         }
 
         .main-image {
-            max-width: 80%;
+            max-width: 500px;
             height: auto;
-            margin-bottom: 30px;
+            margin-bottom: 20px;
             border: 5px;
             border-radius: 10px;
         }
@@ -48,8 +48,8 @@
     </style>
 </head>
 <body>
-<img src="https://thumb.ac-illust.com/65/65e5ff760d2273a88a933c6033f81920_t.jpeg" alt="カーズ" class="main-image">
-    <h1> レース！</h1>
+<img src="https://sdmntprwestus.oaiusercontent.com/files/00000000-3964-6230-ab4f-a2d90caee50e/raw?se=2025-04-24T05%3A09%3A31Z&sp=r&sv=2024-08-04&sr=b&scid=c853af93-4ebc-5168-be17-7a6afc202bd7&skoid=acefdf70-07fd-4bd5-a167-a4a9b137d163&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-24T01%3A18%3A12Z&ske=2025-04-25T01%3A18%3A12Z&sks=b&skv=2024-08-04&sig=ADVXDWgyWTX1AmZTe4Qzxv45jzK9Q4VHxASauXIjZn8%3D" alt="カーズ" class="main-image">
+    <h1>スピードレース・ダービー</h1>
     <form action="../controller.php" method="post">
         <button type="submit" name="first">🚘ゲームスタート🚘</button>
     </form>
