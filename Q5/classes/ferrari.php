973a929aa510df0cbc85e92a60f80cbb154f5e8d
utenma <tenma.ueno@tokyo.supersoftware.co.jp>
2025-04-24 19:00:33 +0900

    動的処理化

--- Q5/classes/ferrari.php
+++ Q5/classes/ferrari.php
@@ -5,14 +5,13 @@ class Ferrari extends Car{
     public $name = "ferrari";
     public $priceRange = ['min'=>3000,'max'=>5000];
     public $vehicleHeight = 100;
-    public $brakeRate = 10;
+    public $brakeRate = 5;
     public $maxSpead = 83.33;
 
     public function __construct(){
 
         $_SESSION[$this->name]['acceleration'] = 6;
         $_SESSION[$this->name]['deceleration'] = 12;
-        $_SESSION[$this->name]['speed'] = min($_SESSION[$this->name]['speed'], $this->maxSpead);
     }
 
 }
--- Q5/classes/honda.php
+++ Q5/classes/honda.php
@@ -4,14 +4,13 @@ require_once('car.php');
 class Honda extends Car{
     public $name = "honda";
     public $priceRange = ['min'=>500,'max'=>600];
-    public $brakeRate = 10;
+    public $brakeRate = 5;
     public $maxSpead = 69.44;
 
     public function __construct(){
 
         $_SESSION[$this->name]['acceleration'] = 4;
         $_SESSION[$this->name]['deceleration'] = 8;
-        $_SESSION[$this->name]['speed'] = min($_SESSION[$this->name]['speed'], $this->maxSpead);
         
     }
 }
